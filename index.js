3edbfdd3846ac037535f3c3765bb6ba8df5bc321
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-01-31 22:04:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-let totalBatteries = batteryBatches.reduce(function,0)
+let totalBatteries = batteryBatches.reduce(function(total,element){return element + total; },0)
 
 let doubledAndSummed = [1, 2, 3].reduce(function(total, element){ return element * 2 + total}, 0)
 // => 12
